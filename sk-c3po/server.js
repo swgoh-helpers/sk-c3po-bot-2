d31234ebf97dc625b61cc4e724d8883b69205b62
sdtbarbarossa <damicha_essen@gmx.de>
2019-06-22 23:05:49 +0200

    little .

--- sk-c3po/server.js
+++ sk-c3po/server.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const spunkte = require('commands/spunkte');
+const spunkte = require('./commands/spunkte');
 
 console.log('starting');
 client.on('ready', () => {
