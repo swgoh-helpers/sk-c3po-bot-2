ca867b9f07f14e9d5028b23ad75a6448672c1ce0
sdtbarbarossa <damicha_essen@gmx.de>
2019-06-22 22:02:59 +0200

    do some error handling

--- sk-c3po/server.js
+++ sk-c3po/server.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log('starting');
+
+console.log("BOT_TOKEN", process.env.BOT_TOKEN);
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
+
+client.on("error", (e) => { console.log("error was thrown by the client!!"); console.error(e); });
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
 
 client.on('message', message => {
 console.log('message received!');
