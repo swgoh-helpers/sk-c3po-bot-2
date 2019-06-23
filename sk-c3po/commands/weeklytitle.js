f0f010c04cc38f7e26a5c90e58cf2bf694d2078e
sdtbarbarossa <damicha_essen@gmx.de>
2019-06-23 12:50:50 +0200

    some tests

--- sk-c3po/commands/weeklytitle.js
+++ sk-c3po/commands/weeklytitle.js
@@ -7,19 +7,61 @@ module.exports = async (message, client) => {
 
         const guild = client.guilds.get(process.env.GUILD_ID);
 
-        let spendengott = message.guild.roles.find('name', 'Spendengott');
+        var report = "";
 
+        //Spendengott
+        let spendengott = message.guild.roles.find('name', 'Spendengott');
         let memberWithRole = spendengott.members;
         memberWithRole.forEach(
             function (mem) {
                 mem.removeRole(spendengott);
             });
-
-        let memberSpendengott = guild.members.find(member => member.user.username.toLowerCase().includes('sdtbarbarossa'));
-
+        let memberSpendengott = guild.members.find(member => member.user.username.toLowerCase().includes('vadith'));
         memberSpendengott.addRole(spendengott);
-        
-        embed.addField("Wochenreport:", `Spendengott: @${memberSpendengott.user}`);
+        report += `Spendengott: ${memberSpendengott.user}\n`;
+
+        //Eifriger Padawan
+        let padawan = message.guild.roles.find('name', 'Eifriger Padawan');
+        padawan.members.forEach(
+            function (mem) {
+                mem.removeRole(padawan);
+            });
+        let memberPadawan = guild.members.find(member => member.user.username.toLowerCase().includes('vadith'));
+        memberPadawan.addRole(padawan);
+        report += `Eifriger Padawan: ${memberPadawan.user}\n`;
+
+        //Staffelführer
+        let staffelfuehrer = message.guild.roles.find('name', 'Staffelführer');
+        staffelfuehrer.members.forEach(
+            function (mem) {
+                mem.removeRole(staffelfuehrer);
+            });
+        let memberStaffelfuehrer = guild.members.find(member => member.user.username.toLowerCase().includes('vadith'));
+        memberStaffelfuehrer.addRole(staffelfuehrer);
+        report += `Staffelführer: ${memberStaffelfuehrer.user}\n`;
+
+        //Großadmiral
+        let großadmiral = message.guild.roles.find('name', 'Großadmiral');
+        großadmiral.members.forEach(
+            function (mem) {
+                mem.removeRole(großadmiral);
+            });
+        let memberGroßadmiral = guild.members.find(member => member.user.username.toLowerCase().includes('vadith'));
+        memberGroßadmiral.addRole(großadmiral);
+        report += `Großadmiral: ${memberGroßadmiral.user}\n`;
+
+        //Gladiator
+        let gladiator = message.guild.roles.find('name', 'Gladiator');
+        gladiator.members.forEach(
+            function (mem) {
+                mem.removeRole(gladiator);
+            });
+        let memberGladiator = guild.members.find(member => member.user.username.toLowerCase().includes('vadith'));
+        memberGladiator.addRole(gladiator);
+        report += `Gladiator: ${memberGladiator.user}\n`;
+
+
+        embed.addField("Wochenreport:", report);
 
         client.channels.get(process.env.WEEKLY_CHANNEL_ID).send({ embed });
         
